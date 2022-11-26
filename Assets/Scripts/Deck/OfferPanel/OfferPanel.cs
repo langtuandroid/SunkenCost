5e771e11c95c5c10f148b53fd4e4230885ab8f0c
bensalter29 <118943810+bensalter29@users.noreply.github.com>
2022-11-27 10:21:45 +1100

    Beginning of Drafting

---
+++ Assets/Scripts/Deck/OfferPanel/OfferPanel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfferPanel : MonoBehaviour
+{
+    public void Hide()
+    {}
+
+    public void Show()
+    {
+        
+    }
+}
